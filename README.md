012887d38a73f3c693a9dfb36d96e187f0e3e9c3
chunterg <chunterg@gmail.com>
2013-05-16 15:50:34 +0800

    package node.exe

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Static server
 =============
 
 Static server by nodejs
+
+## Features
+
+  * Based on Express
+  * Support ajax request map
+  * Support less compile
+  * Support markdown
+
+## How to use
+1.Set the request url which you want to debug in config.json
--- start.js
+++ start.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
-var express = require('../node/node_modules/express');
+var express = require('./sys/node_modules/express');
 var app = express();
-
 //include request-map module
-var req_map = require('./modules/request-map/ajax');
-
+var req_map = require('./lib/request-map/ajax');
 //include file walk module
-var file_walk = require('./modules/file-walk/walk');
+var file_walk = require('./lib/file-walk/walk');
+//include file walk module
+var pre = require('./lib/pre-work/compile');
+
 //load server config
 var cfg = fs.readFileSync(__dirname + '/config.json', 'utf8');
 cfg = JSON.parse(cfg);
@@ -16,32 +17,26 @@ app.configure(function() {
 	app.set('view engine', 'jade');
 	app.use(express.methodOverride());
 	app.use(express.bodyParser());
-	app.use(express.static(cfg.root, {
-		maxAge: 86400000
+	app.use(pre.compile(cfg))	
+	app.use(express.static(cfg.root,{
+		maxAge:!cfg.debug?604800:0
 	}));
 	app.use(express.errorHandler({
 		dumpExceptions: true,
 		showStack: true
 	}));
 	app.use(app.router);
+	app.use(file_walk.walk(cfg))
+
 });
+
 app.all('/ajax/:action', function(req, res) {
 	req_map.ajax(req, res);
 })
-app.use(function(req, res, next) {
-	if (req.originalUrl !== '/ajax/') {
-		var str = file_walk.scan(cfg.root + req.originalUrl);
-		res.render('layout', {
-			_root: req.get('host'),
-			title: 'Node static server',
-			h1: cfg.root + req.originalUrl,
-			body: str
-		});
-	}
-	res.end();
 
-});
 
 app.listen(cfg.port);
 console.log('server start');
-exec("start http://localhost:" + cfg.port + "/");
+
+//console.log(process.memoryUsage())
+//exec("start http://localhost:" + cfg.port + "/");
