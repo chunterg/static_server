a3ba7eb37e62afbd7a66cb384a40558047983360
chunterg <chunterg@gmail.com>
2013-05-23 16:57:49 +0800

    add support for gbk

--- start.js
+++ start.js
@@ -14,21 +14,33 @@ app.configure(function() {
 	app.use(express.methodOverride());
 	app.use(express.bodyParser());
 	//load middleware plugins
-	for(var i =0;i<cfg.middlewares.length;i++){
-		app.use(require(cfg.middlewares[i]).run(cfg))
+	if(cfg.middlewares.length>0){
+		for(var i =0;i<cfg.middlewares.length;i++){
+			app.use(require(cfg.middlewares[i]).run(cfg))
+		}
 	}
+	app.use(app.router);
+
 	//set static file parse
 	app.use(express.static(cfg.root,{
 		maxAge:!cfg.cache?604800:0
 	}));
-	app.use(express.errorHandler({
+	
+	app.use(file_walk.walk(cfg))
+	
+});
+app.configure('development', function(){
+  app.use(express.errorHandler({
 		dumpExceptions: true,
 		showStack: true
 	}));
-	app.use(app.router);
-	app.use(file_walk.walk(cfg))
 });
-
+//router
+app.get('*.html|*.htm',function(req, res) {
+	var fileContent = fs.readFileSync(cfg.root + req.originalUrl);
+    res.charset = cfg.encode;
+    res.end(fileContent);
+});
 
 app.listen(cfg.port);
 console.log('server start');
